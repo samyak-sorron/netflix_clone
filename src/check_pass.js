53692922d93372d304670008c03208c3320841ac
samyak <samyak.sharma.758@gmail.com>
2022-12-05 10:04:34 +0530

    user addded with validation

--- src/check_pass.js
+++ src/check_pass.js
@@ -1,9 +1,13 @@
-function CheckPassword(inputtxt) 
+function CheckPassword(inputtxt,username) 
 { 
+    
     var x=document.getElementById("wrngpass")
     var passw=  /^[A-Za-z]\w{7,14}$/;
     if(inputtxt.value.match(passw)) 
     { 
+        if(username=="admin@gmail.com" && pass=="Admin123"){
+            alert('Good Job! move forward......')
+        }
         alert('Correct, try another...')
         return true;
     }
